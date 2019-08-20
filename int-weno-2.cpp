fbf435e2624068d526474bb74823cf7d80b8d7bb
sugita <sugita5019@gmail.com>
2019-08-20 08:37:13 +0800

    updated smoothness indicator

--- int-weno-2.cpp
+++ int-weno-2.cpp
@@ -67,21 +67,21 @@ float flux1(float stn[5]) //calculate the right-hand side
   p3 = (3.0/8.0)*stn[2]+(3.0/4.0)*stn[3]+(-1.0/8.0)*stn[4];
 
   /*Calculating smoothness indicator */
-  float b1 = 0.3333*(4.0*stn[0]*stn[0]
+  double b1 = 0.3333*(4.0*stn[0]*stn[0]
 	       -19.0*stn[0]*stn[1]
 	       +25.0*stn[1]*stn[1]
 	       +11.0*stn[0]*stn[2]
 	       -31.0*stn[1]*stn[2]
 	       +10.0*stn[2]*stn[2]);
 
-  float b2 = 0.3333*(4.0*stn[1]*stn[1]
+  double b2 = 0.3333*(4.0*stn[1]*stn[1]
 	       -13.0*stn[1]*stn[2]
 	       +13.0*stn[2]*stn[2]
 	       +5.0*stn[1]*stn[3]
 	       -13.0*stn[2]*stn[3]
 	       +4.0*stn[3]*stn[3]);
 
-  float b3 = 0.3333*(10.0*stn[2]*stn[2]
+  double b3 = 0.3333*(10.0*stn[2]*stn[2]
 	       -31.0*stn[2]*stn[3]
 	       +25.0*stn[3]*stn[3]
 	       +11.0*stn[2]*stn[4]
@@ -89,15 +89,18 @@ float flux1(float stn[5]) //calculate the right-hand side
 	       +4.0*stn[4]*stn[4]);
 
   
- float wtilde1 = gamma1/(1.0e-6 + b1)*(1.0e-6 + b1);
- float wtilde2 = gamma2/(1.0e-6 + b2)*(1.0e-6 + b2);
- float wtilde3 = gamma3/(1.0e-6 + b3)*(1.0e-6 + b3);
+  double wtilde1 = gamma1/((1.0e-6 + b1)*(1.0e-6 + b1));
+  double wtilde2 = gamma2/((1.0e-6 + b2)*(1.0e-6 + b2));
+  double wtilde3 = gamma3/((1.0e-6 + b3)*(1.0e-6 + b3));
   
 
- float w1 = wtilde1/(wtilde1+wtilde2+wtilde3);
- float w2 = wtilde2/(wtilde1+wtilde2+wtilde3);
- float w3 = wtilde3/(wtilde1+wtilde2+wtilde3);
-    
+ double w1 = wtilde1/(wtilde1+wtilde2+wtilde3);
+ double w2 = wtilde2/(wtilde1+wtilde2+wtilde3);
+ double w3 = wtilde3/(wtilde1+wtilde2+wtilde3);
+
+ double eps= 1.0e-6;
+ 
+ cout<<wtilde1<<" "<<wtilde2<<" "<<wtilde3<<" "<<eps <<endl;
 
   fr = w1*p1 + w2*p2 + w3*p3; // flux at xi+1/2
  
@@ -140,11 +143,11 @@ int main()
   fx.resize(imax);
   for(i=0; i<imax; ++i){
     x[i] = i*dx;
-        fx[i] = 0.0;
-    if(x[i]>=0.25*pi and x[i]<=0.75*pi) fx[i]=1.0; //function with jump
+    //     fx[i] = 0.0;
+    //if(x[i]>=0.25*pi and x[i]<=0.75*pi) fx[i]=1.0; //function with jump
 
-    //fx[i] = 1.0;
-    //if(x[i]>=0.5*pi) fx[i]=0.0; //function with jump
+    fx[i] = 1.0;
+    if(x[i]>=0.5*pi) fx[i]=0.0; //function with jump
     
   }
   fp.open("ref.csv",ios::out);
@@ -170,8 +173,8 @@ int main()
 
     xg = x[i] + 0.5*dx;
     //fr = flux2(stn);
-    //fr = flux1(stn);
-    fr = eno(stn);
+    fr = flux1(stn);
+    //fr = eno(stn);
 
     xp.push_back(xg);
     fint.push_back(fr);
@@ -192,8 +195,8 @@ int main()
 
     xg = x[i] - 0.5*dx;
     //fr = flux2(stn);
-    //fr = flux1(stn);
-    fr = eno(stn);
+    fr = flux1(stn);
+    //fr = eno(stn);
 
     xl.push_back(xg);
     fl.push_back(fr);
@@ -233,8 +236,8 @@ int main()
     xg = x[i] + 0.5*dx; // exact position
 
     //fleft = flux2(stn);
-    //fleft = flux1(stn);
-    fleft = eno(stn);
+    fleft = flux1(stn);
+    //fleft = eno(stn);
     
     stn[0] = fx[i+3];
     stn[1] = fx[i+2];
@@ -243,8 +246,8 @@ int main()
     stn[4] = fx[i-1];
 
     //fright = flux2(stn);
-    //fright = flux1(stn);
-    fright = eno(stn);
+    fright = flux1(stn);
+    //fright = eno(stn);
 
     ave = 0.5*(fleft + fright);
 
