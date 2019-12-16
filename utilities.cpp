da6f1cae41503d060a7c288b63bd2e7c20cf00be
sugita <sugita5019@gmail.com>
2019-12-16 19:52:35 +0800

    added

--- utilities.cpp
+++ utilities.cpp
@@ -47,3 +47,44 @@ float rkstep(float u, float dt,
   u = u + alpha*dt*source;
   return u;
 }
+
+//WENO5 reconstruction procedure.
+float weno_recon(float stn[5]) //calculate the right-hand side
+{
+  float fr;
+
+  float gamma1, gamma2, gamma3;
+  gamma1 = 0.1f;
+  gamma2 = 0.6f;
+  gamma3 = 0.3f;
+    
+  float p1, p2, p3;
+
+  p1 = (1.0/3.0)*stn[0]+(-7.0/6.0)*stn[1]+(11.0/6.0)*stn[2];
+  p2 = (-1.0/6.0)*stn[1]+(5.0/6.0)*stn[2]+(1.0/3.0)*stn[3];
+  p3 = (1.0/3.0)*stn[2]+(5.0/6.0)*stn[3]+(-1.0/6.0)*stn[4];
+
+  /*Calculating smoothness indicator */
+  float b1 = (13.0/12.0)*(stn[0]-2.0*stn[1]+stn[2])*(stn[0]-2.0*stn[1]+stn[2])
+    +0.25*(3.0*stn[0]-4.0*stn[1]+stn[2])*(3.0*stn[0]-4.0*stn[1]+stn[2]);
+  
+  float b2 = (13.0/12.0)*(stn[1]-2.0*stn[2]+stn[3])*(stn[1]-2.0*stn[2]+stn[3])
+    +0.25*(3.0*stn[1]-4.0*stn[2]+stn[3])*(3.0*stn[1]-4.0*stn[2]+stn[3]);
+  
+  float b3 = (13.0/12.0)*(stn[2]-2.0*stn[3]+stn[4])*(stn[2]-2.0*stn[3]+stn[4])
+    +0.25*(3.0*stn[2]-4.0*stn[3]+stn[4])*(3.0*stn[2]-4.0*stn[3]+stn[4]);
+  
+    
+ float wtilde1 = gamma1/( (1.0e-6 + b1)*(1.0e-6 + b1) );
+ float wtilde2 = gamma2/( (1.0e-6 + b2)*(1.0e-6 + b2) );
+ float wtilde3 = gamma3/( (1.0e-6 + b3)*(1.0e-6 + b3) );
+  
+
+ float w1 = wtilde1/(wtilde1+wtilde2+wtilde3);
+ float w2 = wtilde2/(wtilde1+wtilde2+wtilde3);
+ float w3 = wtilde3/(wtilde1+wtilde2+wtilde3);
+    
+  fr = w1*p1 + w2*p2 + w3*p3;
+  
+  return fr;
+}
